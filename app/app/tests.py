dad615f4f7a9a2ffabfcaa1e2b79afcd7d134bfe
Lucas Bittencurt <lucas@thebittencode.com>
2024-11-01 16:11:54 -0300

    chore: fix linting errors and create wait_for_db command

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,16 +7,16 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-  """Test the calc module."""
+    """Test the calc module."""
 
-  def test_add_numbers(self):
-    """Test adding numbers."""
-    res = calc.add(5, 6)
+    def test_add_numbers(self):
+        """Test adding numbers."""
+        res = calc.add(5, 6)
 
-    self.assertEqual(res, 11)
+        self.assertEqual(res, 11)
 
-  def test_subtract_numbers(self):
-    """Test subtracting numbers."""
-    res = calc.subtract(10, 15)
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
 
-    self.assertEqual(res, 5)
+        self.assertEqual(res, 5)
